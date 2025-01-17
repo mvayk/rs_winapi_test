eb2240e7006dcd72d6255bfee6283c199670f858
mvayк <45300955+mvayk@users.noreply.github.com>
2025-01-17 11:43:12 +1100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let title: Vec<u16> = "title\0".encode_utf16().collect();
     let message: Vec<u16> = "message\0".encode_utf16().collect();
 
-    // classic microsoft
+    // is this necessary
     unsafe {
         winapi::um::winuser::MessageBoxW(std::ptr::null_mut(), message.as_ptr(), title.as_ptr(), winapi::um::winuser::MB_OK | winapi::um::winuser::MB_ICONINFORMATION);
     }
